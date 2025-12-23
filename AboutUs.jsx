fcfb78e6e7fa473680f2ea2633727ba7ff1bca08
girish-kumar-sain <119300691+girish-kumar-sain@users.noreply.github.com>
2025-12-24 00:11:04 +0530

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,13 @@
+export default function AboutUs() {
+  return (
+    <div className="page">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides premium indoor plants that enhance wellness,
+        purify air, and beautify living spaces. Our mission is to bring nature
+        closer to your home.
+      </p>
+    </div>
+  );
+}
 
